256f96a2241899ead2d84a9144014ac4933eed9a
Teo Marcelo <tmarcelojr@gmail.com>
2022-02-22 14:49:44 -0600

    delete and update crud

--- src/main/java/com/teomarcelo/backend/controllers/StudentController.java
+++ src/main/java/com/teomarcelo/backend/controllers/StudentController.java
@@ -3,9 +3,14 @@ package com.teomarcelo.backend.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +44,7 @@ public class StudentController {
 	
 	@GetMapping("allstudents")
 	public List<Student> getAllStudents() {
+//		You can pass on some sorting logic inside findAll() 
 		return studentRepo.findAll();
 	}
 	
@@ -60,6 +66,40 @@ public class StudentController {
 		return studentRepo.findByFirstname(firstname);
 	}
 	
+//	Saves a given entity. Create student
+	@PostMapping("addstudent")
+	public Student newStudent(@RequestBody Student student) {
+		return studentRepo.save(student);
+	}
+	
+	
+	@PutMapping("student/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student newStudentInfo) {
+		Student foundStudent = studentRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Student not found."));
+		
+//		Update info of found student using setters with the new info from req.body using getters.
+		foundStudent.setFirstname(newStudentInfo.getFirstname());
+		foundStudent.setLastname(newStudentInfo.getLastname());
+		
+		Student updatedStudent = studentRepo.save(foundStudent);
+		
+		return ResponseEntity.ok(updatedStudent);
+	}
+	
+//	Use PathVariable to grab the value of {id} parameter from URI
+	@DeleteMapping("student/{id}")
+	public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+//		Find user we want to delete
+		studentRepo.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("Student not found."));
+		
+		String message =  "Student has been deleted.";
+		
+//		Delete method from Jpa. Deletes entity from database.
+		studentRepo.deleteById(id);
+		return new ResponseEntity<>(message, HttpStatus.OK);
+	}
 }
 
 
